ee0f587b31f05479e369dc86bfeeb8c1520b4621
Randy Coulman <rcoulman@gmail.com>
2015-10-22 15:20:14 -0700

    Clean out boilerplate comments

--- DoubleTime/DoubleTime/DoubleTime.h
+++ DoubleTime/DoubleTime/DoubleTime.h
@@ -1,19 +1,4 @@
-//
-//  DoubleTime.h
-//  DoubleTime
-//
-//  Created by Randy Coulman on 10/22/15.
-//  Copyright © 2015 Zeal, LLC. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
-
-//! Project version number for DoubleTime.
 FOUNDATION_EXPORT double DoubleTimeVersionNumber;
-
-//! Project version string for DoubleTime.
 FOUNDATION_EXPORT const unsigned char DoubleTimeVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <DoubleTime/PublicHeader.h>
-
-
